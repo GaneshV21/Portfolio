1970927999bc9c3fb2bd7fbe89116d967d088eca
GaneshV21 <ganesh.v@zybisys.com>
2026-02-08 19:32:25 +0530

    working on profile section

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,10 +1,22 @@
-import React from "react";
-import PropTypes from "prop-types";
+import { Button } from "antd";
 
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div className="flex justify-between p-4">
+      <div>
+        <p className="text-white text-4xl font-bold">{"Hello, I'm"}</p>
+        <p className="text-white text-4xl font-bold">Ganesh</p>
+        <p className="text-white text-sm">
+          qwertyuioplkj sfgdhfjgkhlgfds asfgdhfjgkh
+        </p>
+        <div>
+          <Button size="small">Hire Me</Button>
+          <Button size="small">Download CV</Button>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 }
-
-Profile.propTypes = {};
 
 export default Profile;
