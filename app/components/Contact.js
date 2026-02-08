a590b34749c00af7d885cae3ce520310eb74d187
GaneshV21 <ganesh.v@zybisys.com>
2026-02-08 12:14:42 +0530

    initial commit

---
+++ app/components/Contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Contact() {
+  return <div>Contact</div>;
+}
+
+Contact.propTypes = {};
+
+export default Contact;
---
+++ app/page.js
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Profile />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
